01b2c15490076ffd640c52739c4a17da047ec88c
thegiter <thegiter@outlook.com>
2015-11-03 22:21:38 +0200

    email test update

--- debug/email.php
+++ debug/email.php
@@ -1,5 +1,5 @@
 <?php
-	if (mail('297154048@outlook.com', 'Email Test', 'This email tests if email is working on the server')) {
+	if (mail('297154048@outlook.com', 'Email Test', 'This email tests if email is working on the server '.$_SERVER['SERVER_NAME'], 'From:no-reply@shps.co.za')) {
 		echo 'email accepted';
 	} else {
 		echo 'email not accepted, email failed';
