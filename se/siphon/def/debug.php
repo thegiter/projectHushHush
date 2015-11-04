6e706a1abc4f2b165974cf3f5e6af0782f3d18c7
thegiter <thegiter@outlook.com>
2015-11-04 23:34:54 +0200

    se debug update

--- se/siphon/def/debug.php
+++ se/siphon/def/debug.php
@@ -3,6 +3,12 @@
 		define('root', '../../../');
 	}
 	
+	$ctt = file_get_contents('http://www.gurufocus.com/term/mktcap/'.$ticker.'/Market%2BCap/');
+	echo $ctt;					
+	preg_match('/data_value"\>CN¥(.+) Mil/', $ctt, $matches);
+	echo json_encode($matches);	
+	echo $matches[0];
+echo $matches[1];	
 	require_once root.'se/siphon/cmm/lib/siphon_data.php';
 	
 	echo json_encode(siphon_stock_def_CNY('SHSE:000403', 1.1, 1, .012));
