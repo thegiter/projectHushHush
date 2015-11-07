a2493db4b79e97e3deb5a37ac433433ae9335541
thegiter <thegiter@outlook.com>
2015-11-07 21:57:19 +0200

    db update

--- se/cmm/alter_tbl.php
+++ se/cmm/alter_tbl.php
@@ -14,10 +14,45 @@
 		} else {//then excute sql query
 			//we assume table already exist
 			if (!@mysql_query('ALTER TABLE shse_defs
+			add (
+				lqom DECIMAL(7, 4),
+				slqom DECIMAL(7, 4),
+				tlqom DECIMAL(7, 4),
+				flqom DECIMAL(7, 4),
+				t4qaom DECIMAL(7, 4),
+				tlomr DECIMAL(7, 4),
+				lqroe DECIMAL(7, 4),
+				slqroe DECIMAL(7, 4),
+				tlqroe DECIMAL(7, 4),
+				flqroe DECIMAL(7, 4),
+				t4qaroe DECIMAL(7, 4),
+				tlroer DECIMAL(7, 4)
+			)')) {
+				die('shse add cols error');
+			}
+			
+			if (!@mysql_query('ALTER TABLE szse_defs
+			add (
+				lqom DECIMAL(7, 4),
+				slqom DECIMAL(7, 4),
+				tlqom DECIMAL(7, 4),
+				flqom DECIMAL(7, 4),
+				t4qaom DECIMAL(7, 4),
+				tlomr DECIMAL(7, 4),
+				lqroe DECIMAL(7, 4),
+				slqroe DECIMAL(7, 4),
+				tlqroe DECIMAL(7, 4),
+				flqroe DECIMAL(7, 4),
+				t4qaroe DECIMAL(7, 4),
+				tlroer DECIMAL(7, 4)
+			)')) {
+				die('szse add cols error');
+			}
+			
+			if (!@mysql_query('ALTER TABLE shse_defs
 			change cppftmr cpfptmr DECIMAL(32, 30)')) {
 				die('shse rename cols error');
 			}
-
 			
 			if (!@mysql_query('ALTER TABLE szse_defs
 			change cppftmr cpfptmr DECIMAL(32, 30)')) {
