bc512bc785c11f330ba5d7dc9b522efa6c1099f5
thegiter <thegiter@outlook.com>
2015-12-17 07:53:42 +0200

    se bug fix

--- se/siphon/cmm/lib/siphon_data.php
+++ se/siphon/cmm/lib/siphon_data.php
@@ -65,25 +65,27 @@
 		return $result;
 	}
 	
-	function rolling_curl($urls, $callback, $custom_options = null) {
+/*	function rolling_curl($urls, $callback, $custom_options = null) {
 		// make sure the rolling window isn't greater than the # of urls
 		$rolling_window = 5;
-		$rolling_window = (sizeof($urls) &lt; $rolling_window) ? sizeof($urls) : $rolling_window;
+		$rolling_window = (sizeof($urls) < $rolling_window) ? sizeof($urls) : $rolling_window;
 
 		$master = curl_multi_init();
 		$curl_arr = array();
 
 		// add additional curl options here
-		$std_options = array(CURLOPT_RETURNTRANSFER =&gt; true,
-		CURLOPT_FOLLOWLOCATION =&gt; true,
-		CURLOPT_MAXREDIRS =&gt; 5);
+		$std_options = [
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_MAXREDIRS => 5
+		];
 		$options = ($custom_options) ? ($std_options + $custom_options) : $std_options;
 
 		// start the first batch of requests
-		for ($i = 0; $i &lt; $rolling_window; $i++) {
+		for ($i = 0; $i < $rolling_window; $i++) {
 			$ch = curl_init();
 			$options[CURLOPT_URL] = $urls[$i];
-			curl_setopt_array($ch,$options);
+			curl_setopt_array($ch, $options);
 			curl_multi_add_handle($master, $ch);
 		}
 
@@ -121,7 +123,7 @@
 		curl_multi_close($master);
 		return true;
 	}
-	
+*/	
 	function projectedIncome($oinir, $opcr, $tlomr, $cap, $debt, $wacodr, $tlroer) {
 		$result = new stdClass;
 
