2f9e9c79465543a5c66c1fce4e7b204837072cdb
thegiter <thegiter@outlook.com>
2016-03-25 14:38:50 +0200

    logo bug fix

---
+++ shared/bgs/tdcubes/html.php
@@ -0,0 +1,19 @@
+<?php
+	if (!defined('root')) {
+		die;
+	}
+?>
+<div id="bg-tlight" class="bg-tlight fade-in-norm opa-0">
+	<div class="dsp-non zi-hud disabled">
+		<div>
+		</div>
+	</div>
+	<div class="dsp-non zi-hud disabled">
+		<div>
+		</div>
+	</div>
+	<div class="dsp-non zi-hud disabled">
+		<div>
+		</div>
+	</div>
+</div>
---
+++ shared/bgs/tdcubes/index.php
@@ -0,0 +1,70 @@
+<?php
+	if (!defined('root')) {
+		define('root', '../../../');
+	}
+
+	require_once root.'shared/cache_ctrl/validate.php';
+	
+	//not using etag, because apache deflate gzip changes it
+	//validate also sets the headers for lastmodified and etag
+	cacheCtrlModule::validate('Fri, 25 Mar 2016 14:42:40 GMT');
+	
+	//must validate first, because validate doesn't care if is get or post
+	//while ajax_chk must be POST
+	require_once root.'shared/ajax_chk/ajax_chk.php';
+	
+	require_once root.'shared/cache_ctrl/front_end.php';
+	
+	require_once root.'shared/cache_ctrl/json.php';
+	
+	$manifest = new stdClass;
+	
+	$manifest->hook = 'bg_tdcubes';
+	
+	$r =& $manifest->rscs;
+	$r = [];
+	
+	//each file is loaded asynchronously
+	//however, each group is installed sequentially
+	//while each file within the group are installed parallel, i.e. installed as they are downloaded
+//grp0
+	$r[0] = [];
+	
+	//images must be defined in a group before css, so that we will replace the url to the cache url
+	$r[0][0] = new stdClass;
+	$r[0][0]->type = 'img';
+	$r[0][0]->url = '/shared/bgs/tlight/imgs/bg_ptn.png';
+//grp1
+	$r[1] = [];
+	
+	//sadly no object literals in php
+	$r[1][0] = new stdClass;
+	$r[1][0]->type = 'cpnList';
+	$r[1][0]->cpns = [];
+	
+	$r[1][0]->cpns[0] = new stdClass;
+	$r[1][0]->cpns[0]->name = 'bg_basic';
+	$r[1][0]->cpns[0]->required = true;
+	//no close, define close true if needed
+	
+	$r[1][1] = new stdClass;
+	$r[1][1]->type = 'link';
+	$r[1][1]->url = '/shared/bgs/tlight/csss/bg.css';
+//grp2
+	$r[2] = [];
+
+	$r[2][0] = new stdClass;
+	$r[2][0]->type = 'html';
+	ob_start();//output buffering
+	require root.'shared/bgs/tlight/html.php';
+	$r[2][0]->html = ob_get_clean();
+//grp3
+	$r[3] = [];
+	
+	$r[3][0] = new stdClass;
+	$r[3][0]->type = 'script';
+	$r[3][0]->url = '/shared/bgs/tlight/jss/bg.js';
+	//define $r[][]->async = false; to turn off async loading for the script
+
+	echo json_encode($manifest);
+?>
