d90d117cdcaca189bae9aca04cb1868f1a84f16b
thegiter <thegiter@outlook.com>
2017-03-11 17:56:52 +0200

    se bug fix

--- se/cmm/batch/jss/batch_s.js
+++ se/cmm/batch/jss/batch_s.js
@@ -106,7 +106,9 @@ shpsCmm.domReady().then(function() {
 			var cell = row.children[idx];
 			var tmp;
 			
-			if (tmp = cell.getElementsByName('def-value')[0]) {
+			//css query selector, because getElementsByName can not be used on elements, only document
+			//returns null if no match found, select decendants
+			if (tmp = cell.querySelector('[name="def-value"]')) {
 				cell = tmp;
 			}
 			
