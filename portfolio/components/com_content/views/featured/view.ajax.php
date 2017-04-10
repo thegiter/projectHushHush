0e1aa75445024d89f891770ffbbfe7cabc71ea11
thegiter <thegiter@outlook.com>
2017-04-10 17:14:41 +0200

    shps update

--- portfolio/components/com_content/views/featured/view.ajax.php
+++ portfolio/components/com_content/views/featured/view.ajax.php
@@ -1,44 +1,46 @@
 <?php
 	defined('_JEXEC') or die;
 	(strpos($_POST['refpg'], '#!portfolio/') === 0) or die;
-	
+
 	class ContentViewFeatured extends JViewLegacy {
 		protected $item = null;
 		protected $items = null;
-	
+
 		function display($tpl=null) {
 			$items = $this->get('Items');
-			
+
 			foreach ($items as $key => &$item) {
 				$images = json_decode($item->images);
 				$urls = json_decode($item->urls);
 				$item->slug = $item->alias ? ($item->id . ':' . $item->alias) : $item->id;
-				
+
 				if (isset($images->image_intro) && !empty($images->image_intro)) {
 					$shpsptfl_ftd[$key]['img_url'] = root.'portfolio/'.htmlspecialchars($images->image_intro);
-					$shpsptfl_ftd[$key]['img_wdt'] = htmlspecialchars($images->image_intro_alt);
-					$shpsptfl_ftd[$key]['img_hgt'] = htmlspecialchars($images->image_intro_caption);
+					// = htmlspecialchars($images->image_intro_alt);
+					$shpsptfl_ftd[$key]['img_cap'] = htmlspecialchars($images->image_intro_caption);
 				}
-				
+
 				$shpsptfl_ftd[$key]['item_link'] = str_replace('/portfolio/', '#!portfolio/#', JRoute::_(ContentHelperRoute::getArticleRoute($item->slug, $item->catid)));
-				
+
 				if ($urls && !empty($urls->urlatext)) {
 					$shpsptfl_ftd[$key]['txt'] = true;
-					
+
 					if ($urls->urlbtext) {
 						$shpsptfl_ftd[$key]['txt_pstn'] = $urls->urlbtext;
 					}
-					
+
 					if ($urls->urlctext) {
 						$shpsptfl_ftd[$key]['txt_wdt'] = $urls->urlctext;
 					}
-					
-					$shpsptfl_ftd[$key]['txt_html'] = require $urls->urlatext;
+
+					ob_start();//output buffering
+					require root.$urls->urlatext;
+					$shpsptfl_ftd[$key]['txt_html'] = ob_get_clean();
 				}
 			}
 
 			$doc = JFactory::getDocument();
-			
+
 			$doc->shpsAjax = $shpsptfl_ftd;
 		}
 	}
---
+++ portfolio/images/articles/dummies/4_txt.php
@@ -0,0 +1,10 @@
+<?php
+    defined('root') or die;
+?>
+<p>
+    This is dummy text.<br/>
+    It can be with or without an image.
+</p>
+<p>
+    It can be over an image or on its own.
+</p>
