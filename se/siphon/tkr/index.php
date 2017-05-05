d433f3e0956eee3c16630d43104dfe0f62bc1b6f
thegiter <thegiter@outlook.com>
2017-05-05 23:29:27 +0200

    se tkr update 2

--- se/siphon/tkr/index.php
+++ se/siphon/tkr/index.php
@@ -2,39 +2,39 @@
 	if (!defined('root')) {
 		define('root', '../../../');
 	}
-	
+
 	require root.'shared/phps/dtdec_x.php';
-	
+
 	echo "\n";
-	
+
 	require root.'shared/phps/heads.php';
 ?>
-		<script src="/shared/jss/common.js" type="text/javascript"></script>
-		
+		<script src="/shared/jss/cmm.js" type="text/javascript"></script>
+
 		<link href="/se/siphon/tkr/csss/st.css" rel="stylesheet" type="text/css" charset="utf-8"/>
 		<link href="/se/cmm/single/csss/single.css" rel="stylesheet" type="text/css" charset="utf-8"/>
-		
+
 		<script src="/se/siphon/tkr/jss/st.js" async="async" type="text/javascript"></script>
-		
+
 		<title>CNY Stock Evaluator</title>
 	</head>
-	
+
 	<body>
 		<h1>
 			CNY Stock Ticker List Siphoner
 		</h1>
-		
+
 		<form id="se-form" method="post">
 			<label>Choose Stock Exchange:</label><input id="se-ipt-shse" type="radio" value="SHSE" name="se"/><label for="se-ipt-shse">SHSE</label><input id="se-ipt-szse" type="radio" value="SZSE" name="se"/><label for="se-ipt-szse">SZSE</label><input id="se-ipt-jse" type="radio" value="JSE" name="se"/><label for="se-ipt-jse">JSE</label><label for="se-ipt-szse">SZSE</label><input id="se-ipt-nyse" type="radio" value="NYSE" name="se"/><label for="se-ipt-nyse">NYSE</label><input id="se-ipt-nas" type="radio" value="Nasdaq" name="se"/><label for="se-ipt-nas">Nasdaq</label><br/>
 			<label for="cursor-ipt">Choose Stock List Cursor (start from which stock):</label><input id="cursor-ipt" name="cursor" value="1" type="number"/><br/>
 			<button type="submit">Load Page</button>
 		</form>
-		
+
 		<iframe id="se-iframe">
 		</iframe>
-		
+
 		<button id="sp-btn">Siphon Page</button>
-		
+
 		<p id="msg-cnr">
 		</p>
 	</body>
--- se/siphon/tkr/jss/st.js
+++ se/siphon/tkr/jss/st.js
@@ -1,50 +1,50 @@
-﻿shpsCmm.domReady().then(function() {
+﻿shpsCmm.domMgr.domReady().then(function() {
 	const MAX_NUM_TKRS = 1000;
 
-	var form = document.getElementById('se-form');
-	var iframe = document.getElementById('se-iframe');
-	
-	var ifWdw = iframe.contentWindow;
-	
-	var msgCnr = document.getElementById('msg-cnr');
-	
-	var se;
-	
+	const form = document.getElementById('se-form'),
+	iframe = document.getElementById('se-iframe');
+
+	const ifWdw = iframe.contentWindow;
+
+	const msgCnr = document.getElementById('msg-cnr');
+
+	let se;
+
 	form.addEventListener('submit', function(evt) {
 		evt.preventDefault();
-		
+
 		var fd = new FormData(form);
-		
+
 		switch (se = fd.get('se')) {
 			case 'SHSE':
 				ifWdw.location.href = 'ld_pg.php?se='+se+'&cursor='+fd.get('cursor');
-				
+
 				break;
 			case 'SZSE':
 				ifWdw.location.href = 'tl/szse.html';
-				
+
 				break;
 			case 'JSE':
 				ifWdw.location.href = 'tl/jse.html';
-				
+
 				break;
 			case 'NYSE':
 				ifWdw.location.href = 'tl/nyse.html';
-				
+
 				break;
 			case 'Nasdaq':
 				ifWdw.location.href = 'tl/nasdaq.html';
-				
+
 				break;
 			default:
 				return false;
 		}
 	});
-	
+
 	var btn = document.getElementById('sp-btn');
 	var trCntr = 0;
 	var tkrCntr = 0;
-	
+
 	function recuringGetTkrs(cnr, tkrsArr) {//for when there are too many tickers
 		//now, we loop through each table row, starting from the second row, and construct our data
 		var theRows = cnr.getElementsByTagName('tr');
@@ -52,28 +52,28 @@
 		for (var i = 0; i < theRows.length; i++) {
 			//for each row, the first cell contains ticker symbol, the 7th contains company name
 			var ticker = {};
-			
+
 			var cells = theRows[i].getElementsByTagName('td');
 
 			var matches = /\s*(\d+)\s*/.exec(cells[0].textContent);
-			
+
 			var name = cells[6].textContent;
-			
+
 			if (matches && /[\S]+/.exec(name)) {//if found digits and name is not empty
 				ticker.ticker = matches[1];
 				ticker.name = name;
-				
+
 				tkrsArr.push(ticker);
 			}
-			
+
 			cnr.removeChild(theRows[i]);
 		}
-		
+
 		if (cnr.getElementsByTagName('tr').length > 0) {
 			recuringGetTkrs(cnr, tkrsArr);
 		}
 	}
-	
+
 	function upload(tickers, se, append) {
 		msgCnr.textContent = 'Uploading...';
 
@@ -84,18 +84,18 @@
 		}
 
 		var appendParam = '';
-		
+
 		if (append) {
 			appendParam = '&append=append';
 		}
-		
+
 		//tickers now contain all the ticker data for the page
 		//we upload the data to server
 		shpsCmm.createAjax('post', 'save_to_db.php', 'tkrs_json='+encodeURIComponent(JSON.stringify(tickers))+'&se='+se+appendParam).then(function(xhr) {
 			//check when server respond if successful
 			if (xhr.responseText == 'success') {
 				msgCnr.textContent = 'Done!';
-				
+
 				if (rest) {
 					upload(rest, se, true);
 				}
@@ -104,56 +104,56 @@
 			}
 		});
 	}
-	
+
 	btn.addEventListener('click', function() {
 		var ifDoc = ifWdw.document;
 		var tickers = [];
-		
+
 		switch (se) {
 			case 'SHSE':
 				//get all tables
 				var tbls = ifDoc.getElementsByTagName('table');
-				
+
 				//loop through to find the right table
 				//the right table has the first table row containing the headings
 				var theTbl;
-				
+
 				forEachNodeItem(tbls, function(tbl) {
 					if (tbl.getElementsByTagName('tr')[0].getElementsByTagName('td')[0].textContent.search('证券代码') != -1) {
 						theTbl = tbl;
 					}
 				});
-				
+
 				//now, we loop through each table row, starting from the second row, and construct our data
 				var theRows = theTbl.getElementsByTagName('tr');
-				
+
 				for (var i = 1; i < theRows.length; i++) {
 					//for each row, the first cell contains a link of ticker symbol, the second contains company name
 					var ticker = {};
-					
+
 					var cells = theRows[i].getElementsByTagName('td');
-					
+
 					ticker.ticker = /\s*(\d+)\s*/.exec(cells[0].getElementsByTagName('a')[0].textContent)[1];
 					ticker.name = cells[1].textContent;
-					
+
 					tickers.push(ticker);
 				}
-				
+
 				upload(tickers, se);
-				
+
 				break;
 			case 'SZSE':
 				//get the table
 				var theTbl = ifDoc.getElementsByTagName('table')[0];
-				
+
 				recuringGetTkrs(theTbl.getElementsByTagName('tbody')[0], tickers);
-				
+
 				upload(tickers, se);
-				
+
 				break;
 			case 'JSE':
 				var trs = ifDoc.getElementsByTagName('table')[1].getElementsByTagName('tbody')[0].children;
-				
+
 				//loop through each row
 				//first row is heading, start from 2nd row
 				for (var i = 1; i < trs.length; i++) {
@@ -163,17 +163,17 @@
 					//create ticker
 					var tkr = /\S+/.exec(tr.children[1].children[0].textContent)[0];
 					var sName = tr.children[0].children[0].textContent;
-					
+
 					if ((tkr.length == 3) && !(/((ZA\d+)|\bPref\b|\bprf\b|\bprefs\b|\bNPL\b|REDEEMABLEPREF|\%|\bETF\b|\d(JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)|(JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)\d)/i.exec(sName))) {
 						tickers.push({
 							ticker: tkr,
 							name: sName
-						});				
+						});
 					}
 				}
 
 				upload(tickers, se);
-				
+
 				break;
 			case 'NYSE':
 			case 'Nasdaq':
@@ -190,7 +190,7 @@
 					//create ticker
 
 					var txt = tr.children[0].textContent;
-					
+
 					if (tr.children[1]) {
 						txt += tr.children[1].textContent;
 					}
@@ -198,7 +198,7 @@
 					txt = txt.replace(/(?:\r\n|\r|\n)/g, ' ');
 
 					matches = /^([A-Z\.]+),"([^"]+)"/.exec(txt);
-					
+
 					if (matches) {
 						var sName = matches[2];
 
@@ -208,17 +208,17 @@
 							//select is a reserved word, therefore must be escaped
 							sName = sName.replace(/\bselect/gi, 'ESCSelect');
 							sName = sName.replace(/\bunion/gi, 'ESCUnion');
-							
+
 							tickers.push({
 								ticker: tkr,
 								name: sName
 							});
-						}						
+						}
 					}
 				}
 
 				upload(tickers, se);
-				
+
 				break;
 			default:
 				return false;
