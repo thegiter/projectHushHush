005a8f0821db8aa29596911a92c2b917a372adf5
thegiter <thegiter@outlook.com>
2017-05-06 12:46:14 +0200

    se tkr update bug fix 2

--- se/siphon/tkr/save_to_db.php
+++ se/siphon/tkr/save_to_db.php
@@ -2,11 +2,11 @@
 	if (!defined('root')) {
 		define('root', '../../../');
 	}
-	
+
 	$tkrs = json_decode($_POST['tkrs_json']);
 	$se = $_POST['se'];
 	$append = (isset($_POST['append']) && $_POST['append'] == 'append') ? true : false;
-	
+
 	switch ($se) {
 		case 'SHSE':
 		case 'SZSE':
@@ -14,25 +14,25 @@
 		case 'NYSE':
 		case 'Nasdaq':
 			$se = strtolower($se);
-			
+
 			break;
 		default:
 			die('invalid stock exchange for table');
 	}
-	
+
 	require_once root.'se/cmm/lib/db.php';
-	
+
 	//establish connection
 	$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
 	if ($mysqli->connect_error) {
 		echo 'Connect Error ('.$mysqli->connect_errno.')'.$mysqli->connect_error;
-	else {//then excute sql query
+	} else {//then excute sql query
 		//clear table
 		if (!$append) {
 			$mysqli->query('TRUNCATE TABLE '.$se.'_tkrs');
 		}
-		
+
 		// for each tkr, insert into table
 		foreach ($tkrs as $tkr) {
 			if (!$mysql->query('INSERT INTO '.$se.'_tkrs(tkr, name) VALUES("'.$tkr->ticker.'", "'.$tkr->name.'") ON DUPLICATE KEY UPDATE name=VALUES(name)')) {
