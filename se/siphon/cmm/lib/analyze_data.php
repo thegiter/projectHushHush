94551d78d2745af876163d2fe97c2fd7b2566f5c
thegiter <thegiter@outlook.com>
2017-08-21 19:46:34 +0200

    se update 7

--- se/siphon/cmm/lib/analyze_data.php
+++ se/siphon/cmm/lib/analyze_data.php
@@ -1382,7 +1382,7 @@
 			if ($at12mni <= 0) {
 				$profitadj = 0;
 			} else {
-				$profitadj = pow($at12mni / self::NI_SZE_STD * .65 + self::$def->tlomr / self::RETURN_GRW_STD * .1 + self::$def->tlroer / self::RETURN_GRW_STD * .1 + self::$def->tlrocr / self::RETURN_GRW_STD * .1 + self::$def->rotaRank / self::ROTA_RANK_STD * .05, 2);
+				$profitadj = pow($at12mni / self::NI_SZE_STD * .65 + self::$def->tlomr / self::RETURN_GRW_STD * .1 + self::$def->tlroer / self::RETURN_GRW_STD * .1 + self::$def->tlrocr / self::RETURN_GRW_STD * .1 + self::$def->rotaRank / self::ROTA_RANK_STD * .05, 2.2);
 			}
 			//end profitability adj
 
