a62f12f8c354b2cda679c900d32c82c4bbf59736
thegiter <thegiter@outlook.com>
2018-08-09 18:18:27 +0200

    se ud 1

--- se/siphon/def/db/single/get_iv.php
+++ se/siphon/def/db/single/get_iv.php
@@ -2,18 +2,18 @@
 	if (!defined('root')) {
 		define('root', '../../../../../');
 	}
-	
+
 	require_once root.'se/cmm/lib/chk_auth.php';
-	
+
 	$_POST['se'] = $_GET['se'];
 	$_POST['tkr'] = $_GET['tkr'];
-	
+
 	//we re-siphon the tkr def
 	//it then returns the json data of def obj
 	$_POST['refresh'] = 'refresh';
 	$_POST['ignore_lu'] = 'ignore';
 	require root.'se/siphon/def/db/batch/siphon.php';
-	
+
 	echo '<table>
 		<tr>
 			<td>
@@ -41,6 +41,20 @@
 				cp
 			</td><td>
 				'.$def->cp.'
+			</td>
+		</tr>
+		<tr>
+			<td>
+				cp
+			</td><td>
+				'.$def->glbRank.'
+			</td>
+		</tr>
+		<tr>
+			<td>
+				cp
+			</td><td>
+				'.$def->prlyv.'
 			</td>
 		</tr>
 	</table>';
