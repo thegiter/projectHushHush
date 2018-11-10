7c828577785820ff52162b004266c566c5f95322
thegiter <thegiter@outlook.com>
2018-11-11 01:18:35 +0200

    se ud 9

--- se/siphon/cmm/lib/analyze_data.php
+++ se/siphon/cmm/lib/analyze_data.php
@@ -344,7 +344,7 @@
 			if ($ppadj > 1) {
 				$ppadj_ovrAmt = ($ppadj - 1);
 
-				$ppadj = (1 - ($ppadj_ovrAmt / ($ppadj_ovrAmt + 7))) * $ppadj_ovrAmt + 1;
+				$ppadj = (1 - ($ppadj_ovrAmt / ($ppadj_ovrAmt + 2))) * $ppadj_ovrAmt + 1;
 				//$ppadj = pow($ppadj_ovrAmt, $ppadj_ovrAmt) * ($ppadj_ovrAmt / 7) + 1;
 			}
 
