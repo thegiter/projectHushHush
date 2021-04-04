87341f93b26f82bd5c54f7f8952e1b031f11b1f1
Minglun Zhu <thegiter@users.noreply.github.com>
2021-04-04 02:07:11 +0200

    se debug ud 1

--- se/siphon/cmm/lib/analyze_data.php
+++ se/siphon/cmm/lib/analyze_data.php
@@ -486,9 +486,10 @@
 
 			$rst->fe = $ev_e->fe;
 
+			$rst->ev_icm = self::estimatedValueIcm($ni, $pigr, $dda, $capE);
 			//a company's value can be negative if it loses money each year
 			//however, for stock valuation it is fine to assume the value is 0, because stock price can not be negative
-			$rst->edp = max(self::estimatedValueIcm($ni, $pigr, $dda, $capE) + $ev_e->ev, 0) / $pso / self::DR;
+			$rst->edp = max($rst->ev_icm + $ev_e->ev, 0) / $pso / self::DR;
 
 			return $rst;
 		}
